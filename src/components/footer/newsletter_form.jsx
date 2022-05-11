465165b0ebd0ee5283759427ce3b38b6b04f392c
lizguroart <dianyguti@gmail.com>
2022-05-10 21:19:54 -0500

    new brach

--- src/components/footer/newsletter_form.jsx
+++ src/components/footer/newsletter_form.jsx
@@ -5,7 +5,7 @@ const NewsletterForm = () => (
     <section className="form_inner">
         <h3>Empecemos a trabajar juntos</h3>
         <label className='input_label_form'>Ingresa tu nombre</label>
-        <input className='input_name_form' placeholder='Nombre'></input>
+        <input className='input_name_form' placeholder='Name'></input>
     </section>
 )
 
